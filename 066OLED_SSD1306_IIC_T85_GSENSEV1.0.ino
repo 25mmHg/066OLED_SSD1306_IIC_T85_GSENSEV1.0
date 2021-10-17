d83366517a3bc808f593c40beca9d28130814a18
25mmHg <23mmhg@gmail.com>
2021-10-17 15:14:05 +0200

    Initial commit

---
+++ 066OLED_SSD1306_IIC_T85_GSENSEV1.0.ino
@@ -0,0 +1,216 @@
+/* OLED G-FORCE COUNTER FOR 
+*
+*                
+*                3V3                   LPF 3Hz
+*                 |
+*                 |
+*             .---o---.    47k     47k     47k     47k
+*             |       |    ___     ___     ___     ___
+*             |MMA7361|o--|___|-o-|___|-o-|___|-o-|___|-o------.
+*             |       |         |       |       |       |      |
+*             '---o---'        ---     ---     ---     ---     |
+*                 |            ---     ---     ---     ---     |
+*                 |             | 220n  | 220n  | 220n  | 220n |
+*                 |             |       |       |       |      |
+*                ===           ===     ===     ===     ===     |
+*                GND           GND     GND     GND     GND     |
+*                                                              |
+*                                                              |
+*                                                              |
+*                3V3                               3V3         |
+*                 |                                 |          |
+*                 |    .---------.         .--------o---.      |
+*                 |  -o|.-------.|o-       |   __   |   |      |
+*                 |  -o||       ||o-  SCL  '-o|° |o-'   |      |  Piezo
+*                 |  -o||       ||o----------o|  |o-----)------'
+*                 |  -o||       ||o----------o|  |o-    |  ___    .---|
+*                 |  -o|'-------'|o-  SDA  .-o|__|o-----)-|___|---|   |
+*                 |  -o|         |o-       |            |      .--|   |
+*                 |  -o|OLED64x48|o--.     | ATTINY    --- 1k  |  '---|
+*                 '---o|         |o- |     |   85      ---     |
+*                      '---------'   |     |       220n |      |
+*                                    |     |            |      |
+*                                   ===   ===          ===    ===
+*                                   GND   GND          GND    GND
+*
+*
+*(created with AACircuit v1.28.7 beta 10/23/16 www.tech-chat.de)
+*/
+
+#include <Arduino.h>
+#include <U8x8lib.h>
+#include "fir.h"
+#define SENSORPIN A1
+#define PIEZOPIN 0
+
+const int16_t MAX_TH = 25;
+const int16_t MIN_TH = -20;
+const uint8_t MAXCOUNTER = 64;
+
+/* OLED Constructor u8x8 Display lib */
+U8X8_SSD1306_64X48_ER_SW_I2C u8x8(/* clock=*/ 3, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE);// ATTiny85
+
+/* short Beep */
+void beep()
+{
+  for (uint8_t i = 0; i < 100; i++)
+  {
+    digitalWrite(PIEZOPIN, !digitalRead(PIEZOPIN));
+    delayMicroseconds(250);
+  }
+}
+
+
+/* Big Number Font  */
+void showDigit(uint8_t digit, uint8_t value)
+{
+  if (value == 0)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*  *");
+    u8x8.drawString(digit, 2, "*  *");
+    u8x8.drawString(digit, 3, "*  *");
+    u8x8.drawString(digit, 4, "*  *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else if (value == 1)
+  {
+    u8x8.drawString(digit, 0, "  * ");
+    u8x8.drawString(digit, 1, " ** ");
+    u8x8.drawString(digit, 2, "* * ");
+    u8x8.drawString(digit, 3, "  * ");
+    u8x8.drawString(digit, 4, "  * ");
+    u8x8.drawString(digit, 5, " ***");
+  }
+  else if (value == 2)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*  *");
+    u8x8.drawString(digit, 2, "   *");
+    u8x8.drawString(digit, 3, "  * ");
+    u8x8.drawString(digit, 4, " *  ");
+    u8x8.drawString(digit, 5, "****");
+  }
+  else if (value == 3)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "   *");
+    u8x8.drawString(digit, 2, "  * ");
+    u8x8.drawString(digit, 3, "   *");
+    u8x8.drawString(digit, 4, "*  *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else if (value == 4)
+  {
+    u8x8.drawString(digit, 0, "  * ");
+    u8x8.drawString(digit, 1, " *  ");
+    u8x8.drawString(digit, 2, "* * ");
+    u8x8.drawString(digit, 3, "****");
+    u8x8.drawString(digit, 4, "  * ");
+    u8x8.drawString(digit, 5, "  * ");
+  }
+  else if (value == 5)
+  {
+    u8x8.drawString(digit, 0, " ***");
+    u8x8.drawString(digit, 1, " *  ");
+    u8x8.drawString(digit, 2, " ** ");
+    u8x8.drawString(digit, 3, "   *");
+    u8x8.drawString(digit, 4, "*  *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else if (value == 6)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*   ");
+    u8x8.drawString(digit, 2, "*** ");
+    u8x8.drawString(digit, 3, "*  *");
+    u8x8.drawString(digit, 4, "*  *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else if (value == 7)
+  {
+    u8x8.drawString(digit, 0, "****");
+    u8x8.drawString(digit, 1, "   *");
+    u8x8.drawString(digit, 2, "  * ");
+    u8x8.drawString(digit, 3, " *  ");
+    u8x8.drawString(digit, 4, " *  ");
+    u8x8.drawString(digit, 5, " *  ");
+  }
+  else if (value == 8)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*  *");
+    u8x8.drawString(digit, 2, " ** ");
+    u8x8.drawString(digit, 3, "*  *");
+    u8x8.drawString(digit, 4, "*  *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else if (value == 9)
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*  *");
+    u8x8.drawString(digit, 2, " ***");
+    u8x8.drawString(digit, 3, "   *");
+    u8x8.drawString(digit, 4, "   *");
+    u8x8.drawString(digit, 5, " ** ");
+  }
+  else
+  {
+    u8x8.drawString(digit, 0, " ** ");
+    u8x8.drawString(digit, 1, "*  *");
+    u8x8.drawString(digit, 2, "  * ");
+    u8x8.drawString(digit, 3, "  * ");
+    u8x8.drawString(digit, 4, "    ");
+    u8x8.drawString(digit, 5, "  * ");
+  }
+}
+
+/* Sampling + HPF */
+int16_t readSensor()
+{
+  while (millis() & 63); //wait 64ms = 16Hz samplerate
+  return (fir_filt(analogRead(SENSORPIN)));
+}
+
+/* Threshold prozessing  */
+bool armIsNotMoved()
+{
+  uint16_t counter = MAXCOUNTER;
+  bool isNotMoved = true;
+  int16_t temp = readSensor();
+  while ((temp >= MAX_TH) || ((counter > 0) &&  (counter < MAXCOUNTER )))
+  {
+    temp = readSensor();
+    counter--;
+    if (temp < MIN_TH)
+    {
+      isNotMoved = false;
+      break;
+    }
+  }
+  return (isNotMoved);
+}
+
+void setup(void)
+{
+  pinMode(PIEZOPIN, OUTPUT);
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_saikyosansbold8_u);
+  u8x8.drawString(1, 2, "HELLO");
+  u8x8.drawString(1, 4, "AGAIN");
+  fir_init();
+  for (uint16_t i = 0; i < 50; i++) readSensor();
+  u8x8.clear();
+}
+
+void loop(void)
+{
+  for (uint8_t i = 0; i < 51; i++)
+  {
+    if (i / 10) showDigit(0, i / 10);
+    showDigit(4, i % 10);
+    while (armIsNotMoved());
+    beep();
+    u8x8.clear();
+  }
+}
---
+++ fir.h
@@ -0,0 +1,43 @@
+/* 
+ *  FIR FILTER NACH QUELLE:
+ *  Grundlagen der digitalen Kommunikationstechnik
+ *  Übertragungstechnik – Signalverarbeitung – Netze 
+ *  Carsten Roppel
+ *  E-Mail: c.roppel@fh-sm.de
+ *  Fachbuchverlag Leipzig, 2006
+ */
+
+/* Anzahl der Filterkoeffizienten */
+const uint8_t nc = 31;
+
+static uint8_t zeiger;
+
+int16_t circular_buffer[nc];
+/* Berechnung der 31 Taps mit folgendem Webtool: https://wirelesslibrary.labs.b-com.com/FIRfilterdesigner FILTERPARAMETER siehe FIRfilter.txt*/
+int16_t coeff[]={4518,-2154,-1807,-1581,-1445,-1373,-1342,-1342,-1360,-1387,-1418,-1450,-1479,-1500,-1513,32767,-1513,-1500,-1479,-1450,-1418,-1387,-1360,-1342,-1342,-1373,-1445,-1581,-1807,-2154,4518}; //HPF 0.5Hz fs=16
+
+/* Multiplikation zweier Q15-Zahlen */
+int16_t mult_q15(int16_t factor1, int16_t factor2) 
+{ 
+ return (int16_t)(((int32_t)factor1 * (int32_t)factor2) >> 15);
+}
+
+/* FIR Filter Initialisierung*/
+void fir_init()
+{
+  for(uint8_t i = 0; i < nc; i++) circular_buffer[i] = 0; 
+}
+
+/* FIR Filter Job */
+int16_t fir_filt(int16_t new_sample)
+{
+  circular_buffer[zeiger] = new_sample; 
+  zeiger = (zeiger + 1) % nc;  
+  int16_t y = 0; 
+  for(uint8_t i = 0; i < nc; i++) 
+  { 
+    uint8_t k = (zeiger + i) % nc; 
+    y += mult_q15(coeff[i], circular_buffer[k]); 
+  }
+  return(y);
+}
